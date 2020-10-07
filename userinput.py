f10c0b924a571c724cd965245a429894bfd44609
Gideon Ekeke <71523437+gideonekekeke@users.noreply.github.com>
2020-10-07 09:54:20 +0100

    Add files via upload

---
+++ userinput.py
@@ -0,0 +1,23 @@
+# user input
+
+name1 = input('enter your name:')
+name2 = input('enter your surname:')
+
+
+
+print(name1 , name2)
+
+num1 = int(input('number of voters in agegunle:'))
+
+num2 = int(input('number of voters on lagos island:'))
+
+num3 = int(input('number of voters in lagos mainland:'))
+
+
+print('sum of ', num1+ num2 + num3)
+
+
+
+
+
+
