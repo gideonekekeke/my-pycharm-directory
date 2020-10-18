8aadaf735cdeecc7733898f52c5cf1c81c391ac2
Gideon Ekeke <71523437+gideonekekeke@users.noreply.github.com>
2020-10-19 00:02:45 +0100

    Add files via upload

---
+++ condition2.py
@@ -0,0 +1,27 @@
+
+#write a programme that prints the highest number of this range [2,8, 0, 6, 16, 14, 1]
+
+#SOLUTION
+# declearing a variabble list
+
+list = [2, 8, 0, 6, 16, 14, 1]
+
+#using the function max() to print the highest number from this list
+
+print("the highest number of this list is :" ,max(list))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
